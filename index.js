5d8a29653e3de986130079f24f28818ab2d19cbf
dungpro2011 <75030627+dungpro2011@users.noreply.github.com>
2022-06-23 15:08:25 +0700

    drawCell and generateWhiteBoard

--- index.js
+++ index.js
@@ -186,4 +186,36 @@ const KEY_CODES = {
 
 const WHITE_COLOR_ID = 7;
 
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+class Board {
+  constructor(ctx) {
+    this.ctx = ctx;
+    this.grid = this.generateWhiteBoard();
+  }
+  
+  generateWhiteBoard() {
+    return Array.from({length: ROWS}, ()=> Array(COLS).fill(WHITE_COLOR_ID));
+  }
+
+  drawCell(x, y, colorId) {
+    this.ctx.fillStyle  = COLOR_MAPPING[colorId] || COLOR_MAPPING[WHITE_COLOR_ID];
+    this.ctx.fillRect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    this.ctx.strokeStyle = '#000';
+    this.ctx.strokeRect(x *BLOCK_SIZE, y*BLOCK_SIZE , BLOCK_SIZE, BLOCK_SIZE);
+  }
+}
+
+let board = new Board(ctx);
+
+console.table(board.drawCell(1,1,7));
+
+
+
+
+
 
